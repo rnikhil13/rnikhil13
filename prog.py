b1e2a08394d0160765d902aae21cfec8a25bafb2
Sai Ram Nikhil Ravuri <nikhilravuri136@gmail.com>
2022-04-29 20:58:47 +0530

    ok

--- prog.py
+++ prog.py
@@ -1,7 +1,4 @@
 print('Hello !!')
 print('Added .py file to this repo')
-<<<<<<< HEAD
 print('Extra line added after branching')
-=======
 print('This line is written in test1 branch')
->>>>>>> test1
